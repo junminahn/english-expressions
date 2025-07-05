0197424899b3f62ed193ffa8e397794add33f811
junminahn <36021827+junminahn@users.noreply.github.com>
2025-07-05 17:22:20 +0000

    Deploying to main from @ english-expressions/english-expressions-website@5325c15995206e3fa56011590bd19966b34ef197 🚀

---
+++ assets/js/7dd3b2a7.ed097822.js
@@ -0,0 +1,11 @@
+"use strict";
+(self["webpackChunkwebsite"] = self["webpackChunkwebsite"] || []).push([[3317],{
+
+/***/ 363:
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"metadata":{"permalink":"/blog/page/5","page":5,"postsPerPage":10,"totalPages":5,"totalCount":47,"previousPage":"/blog/page/4","blogDescription":"Blog","blogTitle":"Blog"}}');
+
+/***/ })
+
+}]);
